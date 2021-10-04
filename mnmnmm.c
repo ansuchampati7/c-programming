e8fac6fd7960437ee49b481ee500231ad3a4decf
ansuchampati7 <itzsansuman777@gmail.com>
2021-10-05 03:49:52 +0530

    h

--- mnmnmm.c
+++ mnmnmm.c
@@ -3,18 +3,20 @@ int main()
 {
     int x,i,z;
     scanf("%d", &x);
-    z=0;
+    z=1;
     for ( i = 2; i < x ; ++i)
     {   
         
         if(x%i==0)
         {
-            printf("%d is not a prime number",x);
-            z+=1;
-            break;
+            z=z + i;
+            if(z==x)
+            {
+                printf("perfect number");
+                break;
+            }
         }
     }
-    if(z==0)
-            printf("%d is a prime number",x);
+    
     
 }

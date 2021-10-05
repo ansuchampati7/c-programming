c77752fe6d1ecff90dec2b374c31c3083d9c9acd
ansuchampati7 <itzsansuman777@gmail.com>
2021-10-06 03:07:36 +0530

    hhj

---
+++ fdn32.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,x,z,y;
+    scanf("%d",&x);
+    i=0;
+    y=0;
+    while(x>0)
+    {
+        z=x%10;
+        y = y + z;
+        ++i;
+        x=x/10;
+    }
+    printf("%d", y);
+    return 0 ;
+    
+}

2f27513819d76bd52e555641f490fae6195a7a65
ansuchampati7 <itzsansuman777@gmail.com>
2021-10-18 17:28:21 +0530

    d

---
+++ bbmx.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int c=1,x,y=0,z=1;
+    scanf("%d", &x);
+    
+    while(c<=x)
+    {
+        while(x-y>0)
+        {
+            printf(" ");
+            ++y;
+        }
+        while(z<=c)
+        {
+            printf("*");
+            ++z;
+        }
+        printf("\n");
+        y=c;
+        z=1;
+        ++c;
+    }
+    
+}

6c8c7e7c81ebda5a8299831c5083a95c42a3bb25
ansuchampati7 <itzsansuman777@gmail.com>
2021-11-15 02:51:32 +0530

    bj\

---
+++ NJB.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int  x[2][2]={{10,20},{30,40}},i,j;
+    int (*ptr)[2];
+    ptr = x;
+    for ( i = 0; i < 2; i++)
+    {
+        for ( j = 0; j < 2; j++)
+        {
+            printf("%d \t", *(*(ptr+ i)+j));
+        } 
+    }
+}
